717821720968ae2b51e6477021171f238b0142fc
Allen <63997543+aaw3@users.noreply.github.com>
2023-09-24 11:49:15 -0500

    Fix django bash script again..

--- docker/django_session_fix.sh
+++ docker/django_session_fix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Migrating database to fix issue..."
-docker exec -it seatstock-django /bin/bash "/home/app/webapp/seatstock_django/manage.py migrate"
+docker exec -it seatstock-django /home/app/webapp/seatstock_django/manage.py migrate
